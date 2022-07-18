e6aa542a7f084f187f5d12accf902cd4d1e41c78
JulioNunesDev <julionunes999@gmail.com>
2022-07-18 00:21:56 -0300

    .

--- os/system.py
+++ os/system.py
@@ -1,4 +1,43 @@
 import os
 
-sistema = os.environ
-print(sistema)
+## TRAS OS CAMINHOS
+#sistema = os.environ
+#print(sistema['USERNAME'])
+#print(os.getcwd())
+
+#Trabalhando com pastas
+
+#novo_caminho = r'C:\Users\Julio Nunes\Documents'
+### PARA MUDAR  O CAMINHO ATUAL
+#os.chdir(novo_caminho)
+
+### PARA LISTAR TODOS OS ARQUIVOS DENTRO DA PASTA OU DIRETORIO
+##print(os.listdir())
+
+## PARA CRIAR UMA PASTA
+#os.mkdir('teste')
+
+#########################################
+
+
+### ABRINDO ARQUIVOS
+#caminho = r'C:\Users\Julio Nunes\Documents'
+#os.startfile(caminho)
+
+### RENOMEIA UM ARQUIVO MAIS TEM QUE TER O CAMINHO EXATO
+#os.rename()
+
+### REMOVENDO ARQUIVOS
+#os.remove()
+
+### walk
+#os.walk(os.getcwd())
+### dirname
+#os.path.dirname(os.getcwd())
+### join
+drive = 'C:'
+usuario = 'Julio Nunes'
+pasta_base = 'Documents'
+caminho = os.path.join(drive,r'\Users', usuario, pasta_base)
+os.chdir(caminho)
+print(os.getcwd())
